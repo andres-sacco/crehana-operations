be87d062231bc14300b0844c5ea9e3a9fdaf684b
asacco <sacco.andres@gmail.com>
2022-01-03 20:11:56 -0300

    feat: final version of clase 9

--- src/test/java/com/crehana/operations/service/OperationServiceTest.java
+++ src/test/java/com/crehana/operations/service/OperationServiceTest.java
@@ -2,12 +2,41 @@ package com.crehana.operations.service;
 
 import com.crehana.operations.dto.OperationDTO;
 import com.crehana.operations.validator.OperationValidator;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.stream.Stream;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class OperationServiceTest {
 
+    @BeforeAll
+    void pre_execution_all() {
+        System.out.println("BeforeAll");
+    }
+
+    @BeforeEach
+    void pre_execution() {
+        System.out.println("BeforeEach");
+    }
+
+    @AfterEach
+    void post_execution() {
+        System.out.println("AfterEach");
+    }
+
+    @AfterAll
+    void post_execution_all() {
+        System.out.println("AfterAll");
+    }
+
     @Test
+    @Disabled
+    @DisplayName("Should return the correct sum of two numbers")
     public void should_return_sum_two_numbers() {
 
         //when
@@ -22,5 +51,47 @@ public class OperationServiceTest {
 
         //then
         Assertions.assertEquals(2d, result.getResult());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideParameters")
+    @DisplayName("Should return the correct sum of two numbers")
+    public void should_return_sum_two_numbers(double numberOne, double numberTwo) {
+
+        //when
+        OperationValidator validator = new OperationValidator();
+        OperationService service = new OperationService(validator);
+
+        //given
+        OperationDTO result = service.sum(numberOne, numberTwo);
+
+        //then
+        Assertions.assertEquals(2d, result.getResult());
+    }
+
+
+    @Test
+    @DisplayName("Should return the correct subtract of two numbers")
+    public void should_return_subtract_two_numbers() {
+
+        //when
+        OperationValidator validator = new OperationValidator();
+        OperationService service = new OperationService(validator);
+
+        Double numberOne = 1d;
+        Double numberTwo = 1d;
+
+        //given
+        OperationDTO result = service.subtract(numberOne, numberTwo);
+
+        //then
+        Assertions.assertEquals(0d, result.getResult());
+    }
+
+    private static Stream<Arguments> provideParameters() {
+        return Stream.of(
+                Arguments.of(1, 1),
+                Arguments.of(0, 2)
+        );
     }
 }
