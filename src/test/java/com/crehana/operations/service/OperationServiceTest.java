1b22b94875e1498b88528828dfe2fb187ea302dc
asacco <sacco.andres@gmail.com>
2022-01-04 21:21:02 -0300

    remove parameterized test

--- src/test/java/com/crehana/operations/service/OperationServiceTest.java
+++ src/test/java/com/crehana/operations/service/OperationServiceTest.java
@@ -4,12 +4,8 @@ import com.crehana.operations.dto.OperationDTO;
 import com.crehana.operations.exception.CrehanaException;
 import com.crehana.operations.validator.OperationValidator;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 
-import java.util.stream.Stream;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class OperationServiceTest {
@@ -48,22 +44,6 @@ public class OperationServiceTest {
 
         Double numberOne = 1d;
         Double numberTwo = 1d;
-
-        //when
-        OperationDTO result = service.sum(numberOne, numberTwo);
-
-        //then
-        Assertions.assertEquals(2d, result.getResult());
-    }
-
-    @ParameterizedTest
-    @MethodSource("provideParameters")
-    @DisplayName("Should return the correct sum of two numbers with different parameters")
-    public void should_return_sum_two_numbers(double numberOne, double numberTwo) {
-
-        //given
-        OperationValidator validator = new OperationValidator();
-        OperationService service = new OperationService(validator);
 
         //when
         OperationDTO result = service.sum(numberOne, numberTwo);
@@ -152,13 +132,5 @@ public class OperationServiceTest {
 
         //then
         Assertions.assertEquals("Operation contains something wrong", exception.getMessage());
-    }
-
-
-    private static Stream<Arguments> provideParameters() {
-        return Stream.of(
-                Arguments.of(1, 1),
-                Arguments.of(0, 2)
-        );
     }
 }
